1edc4f5b5c8d6346e66e237284c5a46a6406334e
wezerg <vivian.grimaux@hotmail.fr>
2023-02-20 19:50:31 +0100

    First component

---
+++ spa/salary-simulator/components/Header.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+</script>
+
+<template>
+    <header>
+        <nav>
+            <NuxtLink to="/">Index</NuxtLink>
+            <NuxtLink to="/login">Login</NuxtLink>
+        </nav>
+    </header>
+</template>
+
+<style>
+
+</style>
