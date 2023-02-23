63314a03f9a11aec835063de06901d4063d17185
wezerg <vivian.grimaux@hotmail.fr>
2023-02-24 00:25:37 +0100

    Improve SEO & Meta tags Stored on nuxt config

--- spa/salary-simulator/nuxt.config.ts
+++ spa/salary-simulator/nuxt.config.ts
@@ -1,7 +1,25 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    // Import SCSS
     css: [
         '@/assets/scss/bootstrap.scss',
         '@/assets/scss/main.scss'
-    ]
+    ],
+    // Meta tag & SEO
+    app: {
+        head: {
+            charset: 'utf-8', 
+            viewport: 'width=device-width, initial-scale=1', 
+            title: 'Dev Salary Simulator', 
+            meta: [
+                { 
+                    name: 'description', 
+                    content: 'Now simulate the salary you deserve according to your computer skills!' 
+                }
+            ],
+            htmlAttrs: {
+                lang: 'en'
+            }
+        }
+    }
 });
