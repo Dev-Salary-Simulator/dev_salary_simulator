4590358d44eec4f796571083154bdec5f82678a2
wezerg <vivian.grimaux@hotmail.fr>
2023-02-23 23:32:38 +0100

    Installation bootstrap

--- spa/salary-simulator/app.vue
+++ spa/salary-simulator/app.vue
@@ -8,14 +8,5 @@
 </template>
 
 <style>
-body, html, #__nuxt{
-  width: 100%;
-  height: 100%;
-  margin: 0;
-  background-color: #161820;
-  color: #FFFFFF;
-}
-*{
-  box-sizing: border-box;
-}
+
 </style>
--- spa/salary-simulator/nuxt.config.ts
+++ spa/salary-simulator/nuxt.config.ts
@@ -1,4 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
-})
+    css: [
+        '@/assets/scss/bootstrap.scss',
+        '@/assets/scss/main.scss'
+    ]
+});
