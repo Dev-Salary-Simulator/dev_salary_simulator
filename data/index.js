059092afa4cbd35d242746357e92c2dbe739a92b
Faez-B <faezbhatti@gmail.com>
2023-03-04 13:49:38 +0100

    Data : index.js début

--- data/index.js
+++ data/index.js
@@ -8,3 +8,15 @@ const cors = require('cors');
 const app = express(); 
 
 app.use(cors())
+
+const dataSchema = new mongoose.Schema({
+    
+});
+
+
+const Data = mongoose.model('Data', dataSchema);
+
+// Connexion à MongoDB
+mongoose.connect("mongodb://localhost:27017/Eval")
+.then ( () => console.log("Successfully connected to MongoDB") )
+.catch( () => console.log("Connection failed") )
