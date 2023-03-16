6b7d20d7e48b326d5e3b1affdced2e9ba6013cad
wezerg <wezerg@DESKTOP-8I7N214>
2023-03-16 16:56:45 +0100

    Set colors palette

--- spa/salary-simulator/pages/login.vue
+++ spa/salary-simulator/pages/login.vue
@@ -11,8 +11,8 @@ const loginForm = async () => {
 <template>
     <main>
         <h1>Hello login</h1>
-        <button class="btn btn-success mx-2" @click="(ev: MouseEvent) => loginForm()">Call without token"</button>
-        <button class="btn btn-success mx-2" @click="(ev: MouseEvent) => autoLogin()">Call with token"</button>
+        <button class="btn btn-primary mx-2" @click="(ev: MouseEvent) => loginForm()">Call without token"</button>
+        <button class="btn btn-primary mx-2" @click="(ev: MouseEvent) => autoLogin()">Call with token"</button>
     </main>
 </template>
 
