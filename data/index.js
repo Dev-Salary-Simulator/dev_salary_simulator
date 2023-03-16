1c949dd7e16dc43f084e81c17a75e6888110172f
Faez-B <faezbhatti@gmail.com>
2023-03-16 21:07:34 +0100

    Route de recherche début. Ajout de sécurité dans le code.

--- data/index.js
+++ data/index.js
@@ -1,15 +1,29 @@
 require("dotenv").config();
 
+const helmet = require('helmet');
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const session = require('express-session');
 
-const { MONGO } = process.env;
+const { MONGO, SECRET } = process.env;
 
 const { Jobs } = require("./model/Jobs");
 
-const app = express(); 
+const app = express();
 
+app.set('host', process.env.IP || '127.0.0.1');
+app.set('port', process.env.PORT || 3000);
+app.disable('x-powered-by'); 
+
+app.use(session({
+    resave: false,
+    saveUninitialized: true,
+    secret: SECRET,
+    cookie: { httpOnly: true, secure: true}
+}));
+
+app.use(express.json());
 app.use(cors());
 
 const router = express.Router();
@@ -26,7 +40,7 @@ mongoose.connect(MONGO)
 router.get('/', async (req, res) => {
     try {
         const jobs = await Jobs.find({}, { _id: 0}).exec();
-        res.json(jobs);
+        return res.json(jobs).status(200);
     } catch (error) {
         console.error("Erreur lors de la récupération.\n" + error);
     }
@@ -35,7 +49,7 @@ router.get('/', async (req, res) => {
 router.get('/titles', async (req, res) => {
     try {
         const titles = await Jobs.find().distinct("title");
-        res.json(titles);
+        return res.json(titles.sort()).status(200);
     } catch (error) {
         console.error("Erreur lors de la récupération.\n" + error);
     }
@@ -44,7 +58,7 @@ router.get('/titles', async (req, res) => {
 router.get('/regions', async (req, res) => {
     try {
         const regions = await Jobs.find().distinct("region");
-        res.json(regions);
+        return res.json(regions.sort()).status(200);
     } catch (error) {
         console.error("Erreur lors de la récupération.\n" + error);
     }
@@ -55,17 +69,45 @@ router.get('/stack', async (req, res) => {
 
     try {
         await Jobs.find().exec()
-        .then(jobs => {
-            jobs.map(job => {
-                job.stack.map(stack => {
-                    if (stack && !stacks.includes(stack.toLowerCase())) {
-                        stacks.push(stack.toLowerCase());
-                    }
+            .then(jobs => {
+                // Pour chaque job, on récupère la stack
+                jobs.map(job => {
+                    // Pour chaque élément de la stack, on vérifie s'il est déjà dans le tableau
+                    job.stack.map(stack => {
+                        if (stack && !stacks.includes(stack.toLowerCase())) {
+                            stacks.push(stack.toLowerCase());
+                        }
+                    })
                 })
             })
-        })
+            .catch(error => console.error("Erreur lors de la récupération.\n" + error))
         ;
-        res.json(stacks.sort());
+
+        return res.json(stacks.sort()).status(200);
+    } catch (error) {
+        console.error("Erreur lors de la récupération.\n" + error);
+    }
+});
+
+router.post("/search", async (req, res) => {
+    // TODO : Middleware pour savoir qui a fait la requête
+    
+    const { title, experience, stack, region } = req.body;
+
+    const regexTitle = new RegExp(title, "i");
+    const regexRegion = new RegExp(region, "i");
+
+    const findObj = {};
+
+    if (regexTitle)  findObj.title = regexTitle;
+    if (regexRegion) findObj.region = regexRegion;
+    if (experience)  findObj.experience = experience;
+    if (stack)       findObj.stack = stack; // TODO : La stack entière de la recherche doit être dans chaque résultat
+
+    try {
+        const jobs = await Jobs.find(findObj, { _id: 0});
+
+        return res.json(jobs).status(200);
     } catch (error) {
         console.error("Erreur lors de la récupération.\n" + error);
     }
