d25dca2e1287b7c4ca3a045adf1f078fc3bb92b7
wezerg <wezerg@DESKTOP-8I7N214>
2023-03-25 13:32:37 +0100

    Ident scss

--- spa/salary-simulator/components/Header.vue
+++ spa/salary-simulator/components/Header.vue
@@ -28,48 +28,48 @@ const {userLogged} = useAuth();
             color: inherit;
         }
     }
-}
-.header-login-btn-container{
-    transition: all ease-in-out 0.5s;
-    display: inline-block;
-    height: 52px;
-    border-radius: 10px;
-    border: 1px solid transparent;
-    background-repeat: no-repeat;
-    background-position: center;
-    background-image: linear-gradient(90deg, #023658 0%, rgba(246, 248, 250, 0.5) 100%, #023658 200%);
-    border-radius: 32px;
-    background-size: 400% 400%;
-    animation: spinBorder 3s linear infinite;
-}
-.header-login-btn{
-    display: block;
-    widows: 100%;
-    font-size: 25px;
-    color: $light;
-    padding: 6px 30px;
-    background: $dark;
-    border-radius: 32px;
-    text-transform: uppercase;
-}
-.header-index-btn-container{
-    transition: all ease-in-out 0.5s;
-    display: inline-block;
-    background-repeat: no-repeat;
-    background-position: center;
-    background-image: linear-gradient(269.99deg, #000000 -13.89%, #0D3652 111.32%);
-    background-size: 400% 400%;
-    padding-bottom: 4px;
-    &:hover{
+    .header-login-btn-container{
+        transition: all ease-in-out 0.5s;
+        display: inline-block;
+        height: 52px;
+        border-radius: 10px;
+        border: 1px solid transparent;
+        background-repeat: no-repeat;
+        background-position: center;
+        background-image: linear-gradient(90deg, #023658 0%, rgba(246, 248, 250, 0.5) 100%, #023658 200%);
+        border-radius: 32px;
+        background-size: 400% 400%;
         animation: spinBorder 3s linear infinite;
     }
-}
-.header-index-btn{
-    font-size: 27px;
-    font-weight: 700;
-    color: $grey;
-    background: $dark;
-    padding: 4px 0;
-    text-transform: uppercase;
+    .header-login-btn{
+        display: block;
+        widows: 100%;
+        font-size: 25px;
+        color: $light;
+        padding: 6px 30px;
+        background: $dark;
+        border-radius: 32px;
+        text-transform: uppercase;
+    }
+    .header-index-btn-container{
+        transition: all ease-in-out 0.5s;
+        display: inline-block;
+        background-repeat: no-repeat;
+        background-position: center;
+        background-image: linear-gradient(269.99deg, #000000 -13.89%, #0D3652 111.32%);
+        background-size: 400% 400%;
+        padding-bottom: 4px;
+        &:hover{
+            animation: spinBorder 3s linear infinite;
+        }
+    }
+    .header-index-btn{
+        font-size: 27px;
+        font-weight: 700;
+        color: $grey;
+        background: $dark;
+        padding: 4px 0;
+        text-transform: uppercase;
+    }
 }
 </style>
