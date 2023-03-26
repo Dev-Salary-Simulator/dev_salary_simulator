05efdff9c5a2b33ae059f1f82edfd4cd279ec9bf
wezerg <wezerg@DESKTOP-8I7N214>
2023-03-26 18:57:20 +0200

    Add blue label

--- spa/salary-simulator/components/Label.vue
+++ spa/salary-simulator/components/Label.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
 defineProps<{
     forInput?: string,
+    blueLabel?: boolean
 }>();
 </script>
 
 <template>
-    <label :for="forInput" class="text-l">
+    <label :for="forInput" :class="`text-l ${blueLabel ? 'blue-label' : ''}`">
         <slot></slot>
     </label>
 </template>
@@ -13,5 +14,11 @@ defineProps<{
 <style lang="scss">
 label{
     margin-bottom: 20px;
+    &.blue-label{
+        color: $primary;
+        text-transform: none;
+        font-weight: 700;
+        text-align: center;
+    }
 }
 </style>
