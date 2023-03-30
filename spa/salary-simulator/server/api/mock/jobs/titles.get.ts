a843459bbef84aa0f6589b40c693a0e256ed8e54
wezerg <wezerg@DESKTOP-8I7N214>
2023-03-30 18:30:27 +0200

    First endpoint in server directory Folder mock for mocking data when dev env

---
+++ spa/salary-simulator/server/api/mock/jobs/titles.get.ts
@@ -0,0 +1,12 @@
+export default defineEventHandler((event) => {
+    return [
+        'Developer front-end',
+        'Developer full stack',
+        'Network architect',
+        'System admin',
+        'UX Designer',
+        'Help desk',
+        'Data scientist',
+        'UI Designer',
+    ]
+})
