3cb8460cfee485e5c8347843e9ed542921037fcb
wezerg <wezerg@DESKTOP-8I7N214>
2023-03-30 18:50:43 +0200

    Add auto route api endpoint

---
+++ spa/salary-simulator/server/api/mock/auth/auto.get.ts
@@ -0,0 +1,37 @@
+export default defineEventHandler<TUser>((event) => {
+    return {
+        id: "dz4ad1",
+        firstname: 'Rudolph',
+        lastname: 'Guinguant',
+        birthday: new Date(1680194520330),
+        currentJob: {
+            id: "4q694da",
+            idUser: "dz4ad1",
+            nameJob: "Software engineer",
+            namesStack: ["Typescript", "React", "Nuxt"],
+            nameRegion: "Paris France",
+            salary: 50000,
+            experience: 4,
+            active: true
+        },
+        oldJobs: [{
+            id: "jt465hr",
+            idUser: "dz4ad1",
+            nameJob: "Developer front-end",
+            namesStack: ["Javascript", "React"],
+            nameRegion: "Les Ulis",
+            salary: 35000,
+            experience: 2,
+            active: false
+        },{
+            id: "v94e1v",
+            idUser: "dz4ad1",
+            nameJob: "Help desk",
+            namesStack: ['Php', 'CSS', 'HTML'],
+            nameRegion: "Evry France",
+            salary: 25000,
+            experience: 1,
+            active: false
+        }]
+    }
+})
