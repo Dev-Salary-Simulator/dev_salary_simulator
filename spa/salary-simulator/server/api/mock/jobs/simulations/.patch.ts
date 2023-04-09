c43deac27fdd0da57f8cf521c724c3fbdb011997
wezerg <wezerg@DESKTOP-8I7N214>
2023-04-09 13:19:41 +0200

    Integrate endpoint for simulations view

---
+++ spa/salary-simulator/server/api/mock/jobs/simulations/.patch.ts
@@ -0,0 +1,21 @@
+export default defineEventHandler<TSavedSimulation>(async (event) => {
+    const body: {id: string, saveName: string} = await readBody(event);
+    return {
+        id: body.id,
+        idUser: "416qd4dz13",
+        saveDate: new Date(1681038476784),
+        saveName: body.saveName,
+        simulation: {
+            averageSalary: 50000,
+            lowestSalary: 34000,
+            highestSalary: 67000,
+            parameters: {
+                nameJob: "Developeur front-end",
+                nameRegion: "Paris France",
+                namesStack: ['Java', 'Kotlin', 'VB.Net'],
+                experience: 5,
+                status: "self-employed"
+            }
+        }
+    }
+})
