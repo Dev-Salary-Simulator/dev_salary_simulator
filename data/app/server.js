f1e184fc1ba90df6e08c21ee3db9994e20c72299
Faez-B <faezbhatti@gmail.com>
2023-04-12 09:31:32 +0200

    REFACTOR : middleware

--- data/app/server.js
+++ data/app/server.js
@@ -5,12 +5,13 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const session = require('express-session');
-const jwt  = require('jsonwebtoken');
 
-const { MONGO, SECRET, JWT_KEY, IP, PORT } = process.env;
+const { MONGO, SECRET, IP, PORT } = process.env;
 
 const { Jobs } = require("./model/Jobs");
 const { User } = require("./model/User");
+
+const checkToken = require("./middleware/checkToken");
 
 const app = express();
 
@@ -37,27 +38,6 @@ mongoose.set('strictQuery', false);
 mongoose.connect(MONGO)
 .then ( () => console.log("Successfully connected to MongoDB") )
 .catch( () => console.log("Connection failed") )
-
-const checkToken = (req, res, next) => {
-    // const token = req.headers['Authorization'];
-    const token = req.headers['x-access-token'];
-
-    // Vérification de l'existence du token
-    if (!token) {
-        return res.status(401).send('Aucun token fourni');
-    }
-
-    // Décodage du token
-    jwt.verify(token, JWT_KEY, (err, decoded) => {
-        if (err) {
-            return res.status(500).send('Erreur lors de la vérification du token');
-        }
-
-        // Si tout est bon, sauvegarder l'id du user pour une utilisation dans d'autres routes
-        req.userId = decoded._id;
-        next();
-    });
-};
 
 // Routes
 router.get('/', async (req, res) => {
